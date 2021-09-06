b74b08011a8f949e3ba3fc7b557f440025aae929
ayleafs <luaqsalt@protonmail.com>
2021-09-05 21:16:47 -0400

    fix: autistic kid has been pulled off the computers

--- src/commands/loop.js
+++ src/commands/loop.js
@@ -5,7 +5,7 @@ export const loop = new CommandBase('loop')
   .setDescription('Loops the current queue')
   .setExecutor(interaction => {
     let { member } = interaction;
-    let player = getPlayer(member);
+    let player = getPlayer(member.guild);
 
     if (!player.isValid(member)) {
       interaction.reply(Messages.denied());
